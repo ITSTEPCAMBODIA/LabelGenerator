fe1a2eca7c625ffb63762f126eb3190fd7bcb7f4
Sreng <tongsreng@itc.edu.kh>
2018-09-06 11:13:46 +0700

    Add project files.

---
+++ Lesson01/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson01
+{
+    public partial class Form1 : Form
+    {
+        private bool CtrlPressed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private String CoordinatesToString(MouseEventArgs e)
+        {
+            return "Mouse coordinates: х=" + e.X.ToString() +
+            "; y=" + e.Y.ToString();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = CoordinatesToString(e);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && CtrlPressed) this.Close();
+            else
+            {
+                if(e.Button == MouseButtons.Left)
+                {
+                    Text = CoordinatesToString(e);
+                    if ((e.X < 10 || e.Y < 10
+                        || e.X > Width - 10 || e.Y > Height - 10))
+                    {
+                        Text += "; Outside rectangle";
+                    }
+                    else if ((e.X > 10 && e.Y > 10
+                       && e.X < Width - 10 && e.Y < Height - 10))
+                    {
+                        Text += "; Inside rectangle";
+                    }
+                    else Text += "; On rectangle";
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Control) this.CtrlPressed = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            this.CtrlPressed = false;
+        }
+    }
+}
---
+++ Lesson01/LabelGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson01
+{
+    public partial class LabelGenerator : Form
+    {
+        MouseEventArgs startPoint;
+        public LabelGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void LabelGenerator_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPoint = e;
+        }
+
+        private void LabelGenerator_MouseUp(object sender, MouseEventArgs e)
+        {
+            Label label = new Label();
+            label.Width = Math.Abs(e.X - startPoint.X);
+            label.Height = Math.Abs( e.Y - startPoint.Y);
+            label.Location = new Point(Math.Min(startPoint.X,e.X)
+                , Math.Min(startPoint.Y,e.Y));
+            label.BackColor = Color.Red;
+            label.MouseDown += new MouseEventHandler(Label_MouseDown);
+            this.Controls.Add(label);
+        }
+        private void Label_MouseDown(object sender, MouseEventArgs e)
+        {
+            Label label = (Label)sender;
+            Text = $"Location: (X={label.Location.X},Y=" +
+                $"{label.Location.Y})";
+        }
+    }
+}
